3c4a7ab3910190e476c0ce91d5a5ce80a8eaf010
Ines <inesgerardursin@gmail.com>
2022-03-03 18:22:27 +0000

    Tests on loading data, updating some parameter functions.

--- R/load_imperial_data.R
+++ R/load_imperial_data.R
@@ -2,35 +2,41 @@
 #'
 #' Takes a long time (a few hours) to download all the data.
 #' Data results in about 5 GBs, 15 million obs.
-#' Maybe filter out before?
+#' Best in test: download.file, with a tryCatch with read.csv in case of server
+#' error. Also extending timeout.
+#' Needs additional work for benchmarking and making sure we are downloading
+#' the same thing every time.
 #'
 #' This function loads the imperial forecasts from their github repo.
 #'
 #' @param warnings Default is false. Whether to give warnings.
 #' @import countrycode
+#' @importFrom RCurl curlSetOpt
+#'
 #' @return All of the imperial SEIR projections.
 #' @export
-load_imperial_data <- function(warnings=FALSE){
+load_imperial_data <- function(warnings = FALSE, timeout = 200) {
+  iso3 <- read.csv("data-raw/countries.csv")
+  colnames(iso3)[1] <- "country_code"
+  iso3 <- iso3[!is.na(iso3)]
 
-  iso3 <-read.csv("data-raw/countries.csv")
-  colnames(iso3)[1]<-"country_code"
-  iso3<-iso3[!is.na(iso3)]
-
-  urls.to.try<-list()
-  for(country in iso3){
-    url<-paste0("https://raw.githubusercontent.com/mrc-ide/global-lmic-reports/master/",
-                country, "/projections.csv")
-    urls.to.try<-append(urls.to.try, url)
+  urls.to.try <- list()
+  for (country in iso3) {
+    url <- paste0(
+      "https://raw.githubusercontent.com/mrc-ide/global-lmic-reports/master/",
+      country, "/projections.csv"
+    )
+    urls.to.try <- append(urls.to.try, url)
   }
-
-  if(warnings==TRUE){
-    df_list <-lapply(urls.to.try, readUrl)
+  RCurl::curlSetOpt(timeout = timeout)
+  if (warnings == TRUE) {
+    df_list <- lapply(urls.to.try[1], readUrl)
   } else {
-    df_list <-suppressMessages(lapply(urls.to.try, readUrl))
+    df_list <- suppressMessages(lapply(urls.to.try[1], readUrl))
   }
 
-  df_list<-df_list[!sapply(df_list,is.null)]
-  all.data <- Reduce(function(x, y) merge(x, y, all=TRUE), df_list)
+  df_list <- df_list[!sapply(df_list, is.null)]
+  all.data <- Reduce(function(x, y) merge(x, y, all = TRUE), df_list)
 
   return(all.data)
 }
@@ -43,22 +49,52 @@ load_imperial_data <- function(warnings=FALSE){
 #'
 #' @return Whatever url is read.
 readUrl <- function(url) {
-
-  out <- tryCatch(read.csv(url),
-                  error=function(cond) {
-                    message(paste("URL does not exist:", url))
-                    message(cond)
-                    return(NA)
-                  },
-                  warning=function(cond) {
-                    message(paste("URL caused a warning:", url))
-                    message(cond)
-                    return(NULL)
-                  },
-                  finally={
-                    message(paste("Processed URL:", url))
-                  }
+  tmp <- tempfile()
+  tryCatch(download.file(url, tmp),
+    error = function(cond) {
+      message(paste("URL does not exist:", url))
+      message(cond)
+      tryCatch(read.csv(url),
+        error = function(cond) {
+          message(paste("URL does not exist:", url))
+          message(cond)
+          return(NA)
+        },
+        warning = function(cond) {
+          message(paste("URL caused a warning:", url))
+          message(cond)
+          return(NULL)
+        },
+        finally = {
+          message(paste("Processed URL:", url))
+        }
+      )
+      return(NA)
+    },
+    warning = function(cond) {
+      message(paste("URL caused a warning:", url))
+      message(cond)
+      tryCatch(read.csv(url),
+        error = function(cond) {
+          message(paste("URL does not exist:", url))
+          message(cond)
+          return(NA)
+        },
+        warning = function(cond) {
+          message(paste("URL caused a warning:", url))
+          message(cond)
+          return(NULL)
+        },
+        finally = {
+          message(paste("Processed URL:", url))
+        }
+      )
+      return(NULL)
+    },
+    finally = {
+      message(paste("Processed URL:", url))
+    }
   )
-
+  out <- read.csv(tmp)
   return(out)
 }
--- R/set_params.R
+++ R/set_params.R
@@ -1,66 +1,12 @@
-#' Sets parameters.
+#' Sets country parameters.
 #'
-#' Will be updated & probably broken down into more functions.
-#' For now, will only load the parameters strictly necessary for my individual calculations.
+#' @param country
+#' @param iso3c
 #'
-#'
-#' @return List of parameters
+#' @return List of country name, iso3c code, and population.
 #' @export
-set_params <- function(country=NULL,
-                       iso3c = NULL,
-
-                       cum_cases = NULL,
-                       # case severity distribution
-                       perc_mildI = NULL,
-                       perc_modI = NULL,
-                       perc_sevI = NULL,
-                       perc_critI = NULL,
-
-                       # length of stay by case severity
-                       # in isolation
-                       stay_mild = NULL,
-                       stay_mod = NULL,
-                       # in hospital
-                       stay_sev = NULL,
-                       stay_crit = NULL,
-
-                       # case fatality rate
-                       IFR_sev = NULL,
-                       IFR_crit = NULL,
-
-                       # HCW
-                       n_hcws = NULL,
-                       perc_hcws_not_covid = NULL,
-                       perc_hcws_treat_covid = NULL,
-                       perc_hcws_screen_covid = NULL,
-                       hcws_per_bed = NULL,
-                       cleaners_per_bed = NULL,
-                       ambulancews_per_bed = NULL,
-                       bioengs_per_bed = NULL,
-
-                       cases_screened_per_hcw_per_day = NULL,
-
-                       n_inf_caregivers_hosp = NULL,
-                       n_inf_caregivers_isol = NULL,
-                       # hospital and care infrastructure
-                       n_hosp_beds = NULL,
-                       perc_beds_not_covid = NULL,
-                       perc_beds_sev_covid = NULL,
-                       perc_beds_crit_covid = NULL,
-
-                       n_hosp_beds_per_care_unit = NULL,
-                       # labs and testing
-                       # oxygen use
-                       perc_crit_inv_mv = NULL,
-                       perc_crit_noninv_mv = NULL,
-
-                       o2_flow_sev = NULL,
-                       o2_flow_crit_inv_mv = NULL,
-                       o2_flow_crit_noninv_mv = NULL) {
-
-  # parameter types:
-  # demography
-  # methodology copied from population.R from mrc-ide squire
+set_country <- function(country=NULL,
+                       iso3c = NULL) {
   ## country route
   if(!is.null(country)) {
     country<-as.character(country)
@@ -68,6 +14,9 @@ set_params <- function(country=NULL,
       stop("Country not found")
     }
     population <- esft::who$population[who$country_name == country]
+    income_group <- esft::who$income_group[who$country_name == country]
+    iso3c<-countrycode::countrycode(country, origin = 'country.name',
+                                    destination = 'iso3c')
   }
 
   # iso3c route
@@ -77,27 +26,92 @@ set_params <- function(country=NULL,
       stop("Iso3c not found")
     }
     population <- esft::who$population[who$country_code == iso3c]
+    income_group <- esft::who$income_group[who$country_code == iso3c]
+    country<-countrycode::countrycode(iso3c, origin = 'iso3c', destination =
+                                        'country.name')
+  }
+
+  country_params<- list(country=country,
+                        iso3c = iso3c,
+                        population=population
+                        income_group=income_group)
+  return(country_params)
+}
+
+#' Set case severity distribution
+#'
+#' @param cum_cases Enter known diagnosed number of cumulative cases currently
+#' in country. Must be at least '1'.
+#' @param prop_mild1
+#' @param prop_modI
+#' @param prop_sevI
+#' @param prop_critI
+#'
+#' @return List of proportions.
+#' @export
+set_cases <- function(cum_cases = NULL,
+                        # case severity distribution
+                        prop_mildI = NULL,
+                        prop_modI = NULL,
+                        prop_sevI = NULL,
+                        prop_critI = NULL) {
+  if(prop_mildI < 0 | prop_modI <0 | prop_sevI <0 | prop_critI <0 ){
+    stop("Proportions must be more than 0.")
+  } else if(prop_mildI > 1 | prop_modI >1 | prop_sevI >1 | prop_critI >1 )){
+    stop("Proportions must be less than 1.")
   }
 
 
-  # current known cases
+
   # if not provided, set the minimum required amount
   if(is.null(cum_cases)){
     cum_cases <- 1
   }
+  if(cum_cases <0){
+    stop("Cumulative cases must be above 0")
+  }
   # case severity distribution
-  if(is.null(perc_mildI)){
-    perc_mildI <-0.4
+  if(is.null(prop_mildI)){
+    prop_mildI <-0.4
   }
-  if(is.null(perc_modI)){
-    perc_modI <-0.4
+  if(is.null(prop_modI)){
+    prop_modI <-0.4
   }
-  if(is.null(perc_sevI)){
-    perc_sevI <-0.15
+  if(is.null(prop_sevI)){
+    prop_sevI <-0.15
   }
-  if(is.null(perc_critI)){
-    perc_critI <-0.05
+  if(is.null(prop_critI)){
+    prop_critI <-0.05
   }
+  ##### check if there's any adjustment ------------------------------------------------------------------------
+  if(prop_mildI + prop_modI + prop_sevI + prop_critI != 1 ){
+    stop("The sum of the proportions of mild, moderate, severe, and critical
+         patients must equal to 1.")
+  }
+
+  case_params<- list(cum_cases = cum_cases,
+                     # case severity distribution
+                     prop_mildI = prop_mildI,
+                     prop_modI = prop_modI,
+                     prop_sevI =  prop_sevI,
+                     prop_critI = prop_critI)
+  return(case_params)
+}
+
+#' Set average lengths of stay by case severity, in days.
+#'
+#' @param stay_mild Length of stay in isolation.
+#' @param stay_mod Length of stay in isolation.
+#' @param stay_sev Length of stay in hospital.
+#' @param stay_crit Length of stay in hospital.
+#'
+#' @return Stay list.
+#' @export
+set_stays <- function(stay_mild = NULL, #in isolation
+                      stay_mod = NULL,
+                      # in hospital
+                      stay_sev = NULL,
+                      stay_crit = NULL) {
 
   # length of stay by case severity in days
   # in isolation
@@ -115,6 +129,28 @@ set_params <- function(country=NULL,
     stay_crit <-2
   }
 
+  if(stay_mild < 0 | stay_mod <0 | stay_sev <0 | stay_crit <0 ){
+    stop("Length of stay in isolation or hospital must be greater than 1.")
+  }
+  # mild and moderate in isolation, severe and critical in hospital
+  stay_params<- list(stay_mild = stay_mild,
+                     stay_mod = stay_mod,
+                     stay_sev = stay_sev,
+                     stay_crit = stay_crit)
+
+  return(stay_params)
+}
+
+#' Set IFRs
+#'
+#' @param IFR_sev
+#' @param IFR_crit
+#'
+#' @return
+#' @export
+set_ifrs <- function(IFR_sev = NULL,
+                     IFR_crit = NULL) {
+
   # case fatality rate
   if(is.null(IFR_sev)){
     IFR_sev <-0.134
@@ -122,7 +158,96 @@ set_params <- function(country=NULL,
   if(is.null(IFR_crit)){
     IFR_crit <-0.5
   }
+  if(IFR_sev < 0 | IFR_crit <0 | IFR_sev>1 | IFR_crit >1){
+    stop("Infection fatality ratios must be greater than 0 and less than 1.")
+  }
 
+  # mild and moderate in isolation, severe and critical in hospital
+  ifrs<- list(IFR_sev = IFR_sev,
+              IFR_crit = IFR_crit)
+
+  return(ifrs)
+}
+
+#' Get HCW counts ------------------------------------------------------------------------  add in more categories? if not, where else to get ?
+#'
+#' @param
+#' @param
+#'
+#' @return
+#' @export
+set_hcw <- function(n_hcws = NULL, # dpendent on country, so is number bioeng etc - might also be dependent on case estimation
+                    iso3c = NULL,
+                    country=NULL,
+                    n_cleaners = NULL, # this gets from staffing tool
+                    # these are assumed ratios per hospital
+                    n_bioeng = NULL,
+                    n_ambulanceworkers=NULL) {
+  # The who did some capping here that I can't figure out. Need to ask Luke.
+  # HCW
+  if(!is.null(country)) {
+    country<-as.character(country)
+    if(!country %in% unique(esft::who$country_name)){
+      stop("Country not found")
+    }
+    n_hcws <- esft::who$doctors[who$country_name == country] +
+      esft::who$nurses[who$country_name == country]
+    iso3c<-esft::who$country_code[who$country_name==country]
+  }
+
+  # iso3c route
+  if(!is.null(iso3c)) {
+    iso3c<-as.character(iso3c)
+    if(!iso3c %in% unique(esft::who$country_code)){
+      stop("Iso3c not found")
+    }
+    n_hcws <- esft::who$doctors[who$country_code == iso3c] +
+      esft::who$nurses[who$country_code == iso3c]
+    country<-esft::who$country_name[who$country_code==iso3c]
+  }
+  if(is.null(perc_hcws_not_covid)){
+    perc_hcws_not_covid <-0.4
+  }
+  if(is.null(perc_hcws_treat_covid)){
+    perc_hcws_treat_covid <-0.53
+  }
+  if(is.null(perc_hcws_screen_covid)){
+    perc_hcws_screen_covid <-0.07
+  }
+  ##### check adds to 1 ------------------------------------------------------------------------
+  # mild and moderate in isolation, severe and critical in hospital
+
+  # maybe add the other HCWs? like the categories, if will pull in from HWFE or WHO tool anyways? ------
+  n_inf_caregivers_hosp = NULL
+  n_inf_caregivers_isol = NULL
+
+  hcw_params<- list(n_hcws = n_hcws,
+                    perc_hcws_not_covid = perc_hcws_not_covid,
+                    perc_hcws_treat_covid = perc_hcws_treat_covid,
+                    perc_hcws_screen_covid = perc_hcws_screen_covid)
+
+  return(hcw_params)
+}
+#' Set HCWs ------------------------------------------------------------------------  add in more categories? if not, where else to get ?
+#'
+#' @param
+#' @param
+#'
+#' @return
+#' @export
+set_hcw <- function(n_hcws = NULL, # dpendent on country, so is number bioeng etc - might also be dependent on case estimation
+                 # input percentages
+                    perc_hcws_not_covid = NULL,
+                 perc_hcws_treat_covid = NULL,
+                 perc_hcws_screen_covid = NULL,
+                 # ratios
+                 n_inf_caregivers_hosp = NULL,
+                 n_inf_caregivers_isol = NULL,
+
+                 n_cleaners = NULL, # this gets from staffing tool
+                 # these are assumed ratios per hospital
+                 n_bioeng = NULL,
+                 n_ambulanceworkers=NULL) {
   # The who did some capping here that I can't figure out. Need to ask Luke.
   # HCW
   if(!is.null(country)) {
@@ -153,8 +278,54 @@ set_params <- function(country=NULL,
   if(is.null(perc_hcws_screen_covid)){
     perc_hcws_screen_covid <-0.07
   }
+  ##### check adds to 1 ------------------------------------------------------------------------
+  # mild and moderate in isolation, severe and critical in hospital
 
-# used with the inputs from the HWFE tool
+  # maybe add the other HCWs? like the categories, if will pull in from HWFE or WHO tool anyways? ------
+  n_inf_caregivers_hosp = NULL
+  n_inf_caregivers_isol = NULL
+
+  hcw_params<- list(n_hcws = n_hcws,
+                     perc_hcws_not_covid = perc_hcws_not_covid,
+                     perc_hcws_treat_covid = perc_hcws_treat_covid,
+                     perc_hcws_screen_covid = perc_hcws_screen_covid)
+
+  return(hcw_params)
+}
+#' Set beds ------------------------------------------------------------------------  need to draw in external
+#'
+#' @param
+#' @param
+#'
+#' @return
+#' @export
+set_beds <- function(n_hosp_beds = NULL,
+                     perc_beds_not_covid = NULL,
+                     perc_beds_sev_covid = NULL,
+                     perc_beds_crit_covid = NULL,
+                     n_hosp_beds_per_care_unit = NULL) {
+
+  bed_params<- list(n_hosp_beds = n_hosp_beds,
+                    perc_beds_not_covid = perc_beds_not_covid,
+                    perc_beds_sev_covid = perc_beds_sev_covid,
+                    perc_beds_crit_covid = perc_beds_crit_covid,
+                    n_hosp_beds_per_care_unit = n_hosp_beds_per_care_unit)
+
+  return(bed_params)
+}
+#' Set HCW distributions ----------------------------------------------------------------------- NEED HWFE INPUTS
+#'
+#' @param
+#' @param
+#'
+#' @return
+#' @export
+get_hcws_per_bed <- function(hcws_per_bed = NULL,
+                 cleaners_per_bed = NULL,
+                 ambulancews_per_bed = NULL,
+                 bioengs_per_bed = NULL) {
+
+  # used with the inputs from the HWFE tool
   # need to add in HWFE tool inputs
   hcws_per_bed # weighted averaged based on capped beds per week - how many bds you have
 
@@ -162,17 +333,44 @@ set_params <- function(country=NULL,
   ambulancews_per_bed
   bioengs_per_bed
 
-  cases_screened_per_hcw_per_day
+  # mild and moderate in isolation, severe and critical in hospital
+  hcw_allocations<- list(hcws_per_bed = hcws_per_bed,
+                    cleaners_per_bed = cleaners_per_bed,
+                    ambulancews_per_bed = ambulancews_per_bed,
+                    bioengs_per_bed = bioengs_per_bed)
 
-  n_inf_caregivers_hosp
-  n_inf_caregivers_isol
-  # hospital and care infrastructure
-  n_hosp_beds
-  perc_beds_not_covid
-  perc_beds_sev_covid
-  perc_beds_crit_covid
+  return(hcw_allocations)
+}
 
-  n_hosp_beds_per_care_unit
+#' Calculate case screened per HCW per day -----------------------------------------------------------------------
+#'
+#' @param
+#' @param
+#'
+#' @return
+#' @export
+get_case_screened_per_hcw_per_day <- function() {
+
+# look at the complex capping again here
+
+  return(cases_screened_per_hcw_per_day)
+}
+
+
+
+#' Set mechanical ventilation parameters ------------------------------------
+#'
+#' @param
+#' @param
+#'
+#' @return
+#' @export
+set_mv_params <- function(perc_crit_inv_mv = NULL,
+                           perc_crit_noninv_mv = NULL,
+
+                           o2_flow_sev = NULL,
+                           o2_flow_crit_inv_mv = NULL,
+                           o2_flow_crit_noninv_mv = NULL) {
   # labs and testing
   # oxygen use
   perc_crit_inv_mv
@@ -181,57 +379,15 @@ set_params <- function(country=NULL,
   o2_flow_sev
   o2_flow_crit_inv_mv
   o2_flow_crit_noninv_mv
-  # equipment use
-  input_params<- list(country=country,
-                iso3c = iso3c,
-                population = population,
-                cum_cases = cum_cases,
-                # case severity distribution
-                perc_mildI = perc_mildI,
-                perc_modI = perc_modI,
-                perc_sevI =  perc_sevI,
-                perc_critI = perc_critI,
 
-                # length of stay by case severity
-                # in isolation
-                stay_mild = stay_mild,
-                stay_mod = stay_mod,
-                # in hospital
-                stay_sev = stay_sev,
-                stay_crit = stay_crit,
+  mv_params<- list(perc_crit_inv_mv = perc_crit_inv_mv,
+                   perc_crit_noninv_mv = perc_crit_noninv_mv,
 
-                # case fatality rate
-                IFR_sev = IFR_sev,
-                IFR_crit = IFR_crit,
+                   o2_flow_sev = o2_flow_sev,
+                   o2_flow_crit_inv_mv = o2_flow_crit_inv_mv,
+                   o2_flow_crit_noninv_mv = o2_flow_crit_noninv_mv)
 
-                # HCW
-                n_hcws = n_hcws,
-                perc_hcws_not_covid = perc_hcws_not_covid,
-                perc_hcws_treat_covid = perc_hcws_treat_covid,
-                perc_hcws_screen_covid = perc_hcws_screen_covid,
-                hcws_per_bed = hcws_per_bed,
-                cleaners_per_bed = cleaners_per_bed,
-                ambulancews_per_bed = ambulancews_per_bed,
-                bioengs_per_bed = bioengs_per_bed,
-
-                cases_screened_per_hcw_per_day = cases_screened_per_hcw_per_day,
-
-                n_inf_caregivers_hosp = n_inf_caregivers_hosp,
-                n_inf_caregivers_isol = n_inf_caregivers_isol,
-                # hospital and care infrastructure
-                n_hosp_beds = n_hosp_beds,
-                perc_beds_not_covid = perc_beds_not_covid,
-                perc_beds_sev_covid = perc_beds_sev_covid,
-                perc_beds_crit_covid = perc_beds_crit_covid,
-
-                n_hosp_beds_per_care_unit = n_hosp_beds_per_care_unit,
-                # labs and testing
-                # oxygen use
-                perc_crit_inv_mv = perc_crit_inv_mv,
-                perc_crit_noninv_mv = perc_crit_noninv_mv,
-
-                o2_flow_sev = o2_flow_sev,
-                o2_flow_crit_inv_mv = o2_flow_crit_inv_mv,
-                o2_flow_crit_noninv_mv = o2_flow_crit_noninv_mv)
-  return(inputparams)
+  return(mv_params)
 }
+
+
