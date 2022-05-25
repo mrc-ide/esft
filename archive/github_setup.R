bb889eccbf801dd7c9df42280562ca66aacb3d54
your username <yourname@email.com>
2022-05-25 12:05:18 +0100

    last bit of transferring repo to mrc-ide
    1. moved all of the drafts into the drafts/ folder
    2. added and updated some small reference dataframes for diagnostics
    3. moved written out parameter setting functions to main R folder from personal repo
    
    to be done;
    - details, sources, documentation on the parameters
    - test writing for the parameter functions

---
+++ archive/github_setup.R
@@ -0,0 +1,56 @@
+# GITHUB SETUP
+#### 1. Sign up at GitHub.com ################################################
+
+## If you do not have a GitHub account, sign up here:
+## https://github.com/join
+
+# ----------------------------------------------------------------------------
+
+#### 2. Install git ##########################################################
+
+## If you do not have git installed, please do so:
+## Windows ->  https://git-scm.com/download/win
+## Mac     ->  https://git-scm.com/download/mac
+## Linux   ->  https://git-scm.com/download/linux
+##             or: $ sudo dnf install git-all
+
+# ----------------------------------------------------------------------------
+
+### 3. Configure git with Rstudio ############################################
+
+## set your user name and email:
+usethis::use_git_config(user.name = "your username", user.email = "yourname@email.com")
+
+## create a personal access token for authentication:
+usethis::create_github_token()
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## set personal access token:
+credentials::set_github_pat("XXXXXXXXXX")
+
+## or store it manually in '.Renviron':
+usethis::edit_r_environ()
+## store your personal access token with: GITHUB_PAT=xxxyyyzzz
+## and make sure '.Renviron' ends with a newline
+
+# ----------------------------------------------------------------------------
+
+#### 4. Restart R! ###########################################################
+
+# ----------------------------------------------------------------------------
+
+#### 5. Verify settings ######################################################
+
+usethis::git_sitrep()
+
+## Your username and email should be stated correctly in the output.
+## Also, the report shoud cotain something like:
+## 'Personal access token: '<found in env var>''
+
+## If you are still having troubles, read the output carefully.
+## It might be that the PAT is still not updated in your `.Renviron` file.
+## Call `usethis::edit_r_environ()` to update that file manually.
+
+# ----------------------------------------------------------------------------
+
+## THAT'S IT!
--- data-raw/data_wrangle.R
+++ data-raw/data_wrangle.R
@@ -1,5 +1,9 @@
 # data wrangling
+library(readxl)
+library(usethis)
+library(countrycode)
 
+# Summary information from the HWO --------------
 who <- readxl::read_excel("data-raw/who_summary_data.xlsx",
                           sheet = "who_summary_data")
 usethis::use_data(who, overwrite = TRUE, internal = FALSE)
@@ -19,28 +23,24 @@ usethis::use_data(bed_perc_crit_proxy, overwrite = TRUE, internal = FALSE)
 hwfe <- readxl::read_excel("data-raw/who_summary_data.xlsx", sheet = "hwfe")
 usethis::use_data(hwfe, overwrite = TRUE, internal = FALSE)
 
+population <- readxl::read_excel("data-raw/who_summary_data.xlsx",
+                                 sheet = "population")
+usethis::use_data(population, overwrite = TRUE, internal = FALSE)
 
-
+# Equipment ratios by HCW and patient ---------------
 equipment <- readxl::read_excel("data-raw/who_summary_data.xlsx",
                                 sheet = "equipment")
 equipment$reusable <- ifelse(equipment$reusable == "No", FALSE, TRUE)
 usethis::use_data(equipment, overwrite = TRUE, internal = FALSE)
 
-transmission_scenarios <- readxl::read_excel("data-raw/who_summary_data.xlsx",
-                                             sheet = "transmission_scenarios")
-usethis::use_data(transmission_scenarios, overwrite = TRUE, internal = FALSE)
 
-population <- readxl::read_excel("data-raw/who_summary_data.xlsx",
-                                 sheet = "population")
-usethis::use_data(population, overwrite = TRUE, internal = FALSE)
-
-# Pharmaceuticals
+# Pharmaceuticals ---------------------
 pharmaceuticals <- readxl::read_excel("data-raw/who_summary_data.xlsx",
                                       sheet = "pharmaceutical")
 pharmaceuticals$covid_specific <- ifelse(pharmaceuticals$covid_specific == "No", FALSE, TRUE)
 usethis::use_data(pharmaceuticals, overwrite = TRUE, internal = FALSE)
 
-# Diagnostics - matching country name to country code
+# Diagnostics ---------------------
 diagnostics <- readxl::read_excel("data-raw/who_summary_data.xlsx",
                                   sheet = "platform_mapping")
 
@@ -58,3 +58,16 @@ subset(diagnostics, is.na(diagnostics$country_code))
 
 diagnostics$country_code[diagnostics$country_name == "Micronesia"] <- "FSM"
 usethis::use_data(diagnostics, overwrite = TRUE, internal = FALSE)
+
+transmission_scenarios <- readxl::read_excel("data-raw/who_summary_data.xlsx",
+                                             sheet = "transmission_scenarios")
+usethis::use_data(transmission_scenarios, overwrite = TRUE, internal = FALSE)
+
+capacity_perc <- readxl::read_excel("data-raw/who_summary_data.xlsx",
+                                             sheet = "capacity_perc")
+usethis::use_data(capacity_perc, overwrite = TRUE, internal = FALSE)
+
+hours_per_shift <- readxl::read_excel("data-raw/who_summary_data.xlsx",
+                                    sheet = "hours_per_shift")
+usethis::use_data(hours_per_shift, overwrite = TRUE, internal = FALSE)
+
