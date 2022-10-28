0ae9d3dab2a0b5ee5b75d461b86a7ba1a0f08bf6
your username <inescgu@gmail.com>
2022-10-28 12:34:40 +0100

    Consolidating functions.

--- R/diagnostic_parameters.R
+++ R/diagnostic_parameters.R
@@ -288,3 +288,39 @@ get_lab_parameters <- function(overrides = list()) {
   }
   parameters
 }
+
+#' @title Given specified parameters, calculates diagnostic country capacity.
+#'
+#' @param country_diagnostic_capacity Capacity called from the
+#' get_country_test_capacity function
+#' @param throughput Throughput data, loaded in package, from ESFT
+#' @param hours_per_shift Hours per shift, default = 8
+#'
+#' @import dplyr
+#' @importFrom magrittr %>%
+#'
+#'
+#' @export
+calc_diagnostic_capacity <- function(country_diagnostic_capacity,
+                                     throughput,
+                                     hours_per_shift) {
+  capacity <- merge(throughput, country_diagnostic_capacity)
+  capacity <- merge(capacity, hours_per_shift, by.x = "shifts_day",
+                    by.y = "shifts")
+
+  capacity <- capacity %>%
+    mutate(throughput_per_day = case_when(
+      hours == 8 ~ throughput_8hrs,
+      hours == 16 ~ throughput_16hrs,
+      hours == 24 ~ throughput_24hrs,
+      TRUE ~ NA_real_
+    )) %>%
+    select(-c(throughput_8hrs, throughput_16hrs, throughput_24hrs))
+
+  # calculate the testing capacity available - max, and covid
+  capacity$total_test_capacity <- capacity$modules_activated * capacity$days_week * capacity$throughput_per_day
+  capacity$covid_test_capacify <- capacity$total_test_capacity * capacity$covid_capacity
+
+
+  return(capacity)
+}
