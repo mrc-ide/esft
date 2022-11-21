40fb922d0e06ab4ff5d64876a87be4570dc32128
your username <inescgu@gmail.com>
2022-11-21 10:26:10 +0000

    creat hcw_tests.R

---
+++ drafts/hcw_tests.R
@@ -0,0 +1,18 @@
+#' HCWs + TESTING
+#' screening_hcw_uncapped # this depends on testing
+#' screening_hcw_capped # also depends on testing
+#' inf_caregiver_isolation # again depends on testing
+#' cleaners_lab_capped <- total_labs*hygienists_per_lab # also depends on diagnostics
+#'
+#' @export
+hcw_tests <- function(hcw_caps,
+                      hcws_weekly,
+                      diagnostics_weekly,
+                      testing_strategy,
+                      max_tests_per_day){
+
+  # additional testing
+  # contact tracing
+  # output based on testing capacity
+  # screening/triage
+}
