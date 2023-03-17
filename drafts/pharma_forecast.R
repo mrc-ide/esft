85b6c221c76e671866331f44be1aa6d787ba2b1e
your username <inescgu@gmail.com>
2023-03-17 11:11:42 +0000

    pharma

--- drafts/pharma_forecast.R
+++ drafts/pharma_forecast.R
@@ -1,17 +1,17 @@
 #' @title Pharmaceuticals Forecast
 #'
-#' Could take the pharma inputs and times them by weekly forecast ratios
-#' Unclear if want to move forward with costing tool though
-#' Maybe I use a hashing tool or a code to deal with pharma names
-#'
 #' @description
+#' Takes the pharma inputs and times them by total new cases.
+#' Unclear how to work out the price setting aspect.
+#'
+#'
 #'
 #' @import dplyr
 #' @importFrom tidyselect everything
 #' @importFrom magrittr %>%
 #'
 #' @export
-pharma_weekly <- function(pharmaceuticals, cases) {
+pharma_forecast <- function(pharmaceuticals, cases) {
 
   sums <- cases %>%
       dplyr::select(-c(week_ends, week_begins)) %>%
@@ -20,11 +20,17 @@ pharma_weekly <- function(pharmaceuticals, cases) {
     dplyr::select(c(new_critical_cases, new_severe_cases, new_mild_cases,
                     new_mod_cases))
 
+  pharmaceuticals[is.na(pharmaceuticals)] <- 0
+
   pharmaceuticals <- pharmaceuticals %>%
     dplyr::mutate(
-      total_drug_form_all_mild_treated = sums$new_mild_cases*,
-      total_drug_form_all_mod_treated = ,
-      total_drug_form_all_severe_treated = ,
+      total_drug_form_all_mild_treated =
+        sums$new_mild_cases*form_per_mild_course*perc_mild_treated,
+      total_drug_form_all_mod_treated =
+        sums$new_mod_cases*form_per_mod_course*perc_mod_treated,
+      total_drug_form_all_severe_treated =
+        sums$new_severe_cases*form_per_sev_course*perc_sev_treated,
       total_drug_form_all_crit_treated =
+        sums$new_critical_cases*form_per_crit_course*perc_crit_treated
     )
 }
